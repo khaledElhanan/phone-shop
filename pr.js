c6d021e6c55bd3e62a39392ccb9451c8941099c3
khaled Abd_Elhanan <121755978+khaledElhanan@users.noreply.github.com>
2023-04-09 12:31:45 +0200

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,9 @@
+let kh = document.querySelector('.kh');
+let khaled = document.querySelector('.khaled');
+function phones(phone) {
+    kh.src = phone;
+
+}
+function colors(color) {
+    khaled.style.background = color;
+}
